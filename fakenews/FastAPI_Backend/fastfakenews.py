ecd1fa0e850d6ff7a7d2a3d30dde71aac75c62e3
Maria Borrell <maria.borrell.r@gmail.com>
2023-02-22 20:04:41 +0100

    now that I removed all incompatibilities but isnt working yet I need to see if it has to do with nklt still

--- fakenews/FastAPI_Backend/fastfakenews.py
+++ fakenews/FastAPI_Backend/fastfakenews.py
@@ -12,9 +12,9 @@ from fakenews.FastAPI_Backend.tokenizercreator import load_tokenizer
 import joblib
 import numpy as np
 import string
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
+#from nltk.tokenize import word_tokenize
+#from nltk.corpus import stopwords
+#from nltk.stem import WordNetLemmatizer
 
 app = FastAPI()
 
@@ -42,11 +42,11 @@ async def make_prediction(text):
     for punctuation in string.punctuation:
         new_title = new_title.replace(punctuation,"")
     new_title = word_tokenize(new_title)
-    stop_words = set(stopwords.words('english'))
-    new_title = [w for w in new_title if not w in stop_words]
-    new_title = [WordNetLemmatizer().lemmatize(w, pos = "v") for w in new_title]
-    new_title = [WordNetLemmatizer().lemmatize(w, pos = "n") for w in new_title]
-    new_title = " ".join(new_title)
+    #stop_words = set(stopwords.words('english'))
+    #new_title = [w for w in new_title if not w in stop_words]
+    #new_title = [WordNetLemmatizer().lemmatize(w, pos = "v") for w in new_title]
+    #new_title = [WordNetLemmatizer().lemmatize(w, pos = "n") for w in new_title]
+    #new_title = " ".join(new_title)
     return {"prova": new_title}
     #tk = load_tokenizer()
     #title_preprocessed_token = tk.texts_to_sequences([title_preprocessed])
