a4868ba2a95ea393291ccc77e5d169e95a43a25c
Maria Borrell <maria.borrell.r@gmail.com>
2023-02-23 16:02:58 +0100

    tried another thing for downloading nltk lol

--- fakenews/FastAPI_Backend/nltk_download_utils.py
+++ fakenews/FastAPI_Backend/nltk_download_utils.py
@@ -1,4 +1,6 @@
-import nltk
-nltk.download('wordnet')
-nltk.download('punkt')
-nltk.download('all-corpora')
+import subprocess
+
+cmd = ["python3", "-m", "textblob.download_corpora"]
+
+subprocess.run(cmd)
+
