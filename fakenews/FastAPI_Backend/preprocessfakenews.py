f5b1e073068bd2e2bafbdd53f5d6257eb6d6c749
Maria Borrell <maria.borrell.r@gmail.com>
2023-02-27 16:46:06 +0100

    did the stopwords step manually

--- fakenews/FastAPI_Backend/preprocessfakenews.py
+++ fakenews/FastAPI_Backend/preprocessfakenews.py
@@ -2,7 +2,7 @@ import string
 #from nltk.tokenize import word_tokenize
 #from nltk.corpus import stopwords
 #from nltk.stem import WordNetLemmatizer
-import spacy
+#import spacy
 
 def preprocess_title(title):
 
@@ -22,9 +22,8 @@ def preprocess_title(title):
     #tokenizing
     title = title.split()
 
-    #removing stopwords
-    sp = spacy.load('en_core_web_sm')            
-    stop_words = sp.Defaults.stop_words
+    #removing stopwords     
+    stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
     title = [w for w in title if not w in stop_words]
 
